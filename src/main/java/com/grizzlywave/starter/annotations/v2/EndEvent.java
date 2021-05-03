102beb98d0bc83d87518a2580e0dcc4887b212d5
Brahim LOUKIL <brahim.loukil@codeonce.fr>
2021-05-03 01:27:11 +0100

    [TECH] add new version of annotation

---
+++ src/main/java/com/grizzlywave/starter/annotations/v2/EndEvent.java
@@ -0,0 +1,16 @@
+package com.grizzlywave.starter.annotations.v2;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD}) 
+@Retention(RetentionPolicy.RUNTIME)
+public @interface EndEvent {
+	
+	String value() default "";
+	
+	String key() default "";
+
+}
